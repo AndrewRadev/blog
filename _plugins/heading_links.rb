99a0a84d6e5373cd0d1dd76f44c1a9741f270ddb
Andrew Radev <andrey.radev@gmail.com>
2019-03-30 11:32:24 +0200

    Heading links

---
+++ _plugins/heading_links.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+#
+class Jekyll::MarkdownHeader < Jekyll::Converters::Markdown
+  def convert(content)
+    super.gsub(/<h(\d) id="(.*?)">/, '<h\1 id="\2"><a class="anchor" aria-hidden="true" href="#\2">🔗</a>')
+  end
+end
